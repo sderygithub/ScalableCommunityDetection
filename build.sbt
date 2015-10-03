4d88ab45836268a2be8aca25323591b9fa675432
Sebastien Dery <sderymail@gmail.com>
2015-10-03 12:42:17 -0700

    Updated on Project Build and Database Setup

--- build.sbt
+++ build.sbt
@@ -9,3 +9,7 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.1" % "provided"
 libraryDependencies += "org.apache.spark" %% "spark-graphx" % "1.4.1"
 
 libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "1.4.0-M3"
+
+libraryDependencies += "com.github.scopt" %% "scopt" % "3.3.0"
+
+resolvers += Resolver.sonatypeRepo("public")
--- src/db/db_api.py
+++ src/db/db_api.py
@@ -1,11 +1,11 @@
 import happybase
-
+import json
 import imp
-import happybase
 
 from flask import Flask
 from flask import render_template
 from flask import request
+
 app = Flask(__name__)
 from cassandra.cluster import Cluster
 
@@ -14,20 +14,55 @@ from cassandra.cluster import Cluster
 def hello():
 	return "Hello world!"
 
+@app.route("/largegraph/")
+def graph_site():
+	return render_template('largegraph.html')
+
 @app.route("/hbase/")
 def hbase_test():
-	# Cassandra uses 7000 for cluster communication 
-	# (7001 if SSL is enabled)
-	# 9160 for Thrift clients
-	# 9042 for native protocol clients
-	# and 7199 for JMX. 
-	#connection = happybase.Connection(host='54.219.144.56', port=9160)
-	#my_table = connection.table('gtest')
-	#key6 = my_table.row(6)
-	#return key6['target']
+	# Establish contact with database
+	cluster = Cluster(contact_points=['54.219.144.56'],)
+	session = cluster.connect('harary')
+	result = session.execute("SELECT * FROM gtest")
 
-	cluster = Cluster()
-	session = cluster.connect('demo')
+	# Empty result scenario
+	if result == None:
+		return  {}
+
+	# In extreme case, we force a drastic truncation
+	if len(result) > 200000:
+		print "SD> WARN: Excessive number of node (%i). Something is probably wrong.." % len(result)
+		result = result[0:1000]
+
+	node_index = 0
+	edge_index = 0
+
+		# Allocate enough for a typical number of supported community
+	expected_number_of_nodes = 1000
+	edges = [{'source':0, 'target':0} for k in range(expected_number_of_nodes * expected_number_of_nodes)]
+	nodes = [{'index':0, 'community':0, 'x':0, 'y':0, 'size':10} for k in range(expected_number_of_nodes)]
+
+	# Add all nodes
+	for node in result:
+		nodes[node_index]['index'] = node.source
+		nodes[node_index]['community'] = node.community
+		node_index = node_index + 1
+		if node.target != None:
+			# Add all edges
+			for target in node.target:
+				edges[edge_index]['source'] = node.source
+				edges[edge_index]['target'] = target
+				edge_index = edge_index + 1
+
+	# Truncate excess
+	nodes = nodes[0:node_index]
+	edges = edges[0:edge_index]
+
+	#with open("/Users/sdery/GitHub/HararyProject/website/static/graphs/dblp.json", 'w') as outfile:
+	#	json.dump({'nodes': nodes, 'links': edges}, outfile)
+
+	# Return json string
+	return json.dumps({'nodes': nodes, 'links': edges})
 
 if __name__ == "__main__":
     app.run(debug=True)
